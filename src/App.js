1b50c8d216ddcdec30ae529702ef1c716005a9e4
yashyashdarji04 <darjiyash73@gmail.com>
2025-08-29 19:36:48 +0530

    feat : change my name to new changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & Node.js
+        new changes
         </a>
       </header>
     </div>
